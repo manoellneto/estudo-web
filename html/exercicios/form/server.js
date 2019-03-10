372249672ee747ebd88b2f5b11a6bec57190da2d
Your Name <m4cneto@gmail.com>
2019-03-10 03:07:43 -0300

    Changes

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: true}))
-
+app.use(bodyParser.urlencoded({extended: true}))
+ 
 app.post('/usuario', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabens</h1>')
+    resp.send('<h1>Usuario incluido.</h1>')
 })
 
+app.post('/usuario/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Usuario alterado.</h1>')
+})
+ 
 app.listen(3003)
